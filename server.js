60266bf506eb97e6f1bb579b4bc8c11b312685bb
Tuly7 <cindetul@students.zhaw.ch>
2023-05-30 18:37:07 +0200

    1. Test

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const express = require('express');
 const PORT = process.env.PORT || 8080; const HOST = '0.0.0.0';
 // App
 const app = express(); app.get('/', (req, res) => {
-res.send('Hello DevOps Course :-)))'); });
+res.send('Hello DevOps Course :-))). Das ist ein Test'); });
 app.listen(PORT, HOST); console.log(`Running on http://${HOST}:${PORT}`);
